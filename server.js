3cba9c06e14844fcbc6393f84d85e9c45ed95b79
Santiago Rodriguez <santiagodrm@gmail.com>
2025-07-25 15:46:05 +0200

    module: Install cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 import taskRoutes from './routes/taskRoutes.js';
 
@@ -6,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors())
 app.use('/user', userRoutes);
 app.use('/task', taskRoutes)
 
