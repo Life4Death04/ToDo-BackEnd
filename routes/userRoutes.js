2f7b966563494410a3cdc6646d04227ab45cc086
Santiago Rodriguez <santiagodrm@gmail.com>
2025-08-20 23:24:07 +0200

    bug: Remove Auth Middleware from Login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,6 +12,6 @@ userRoutes.patch('/updatePassword', updateUserPasswordController)
 userRoutes.delete('/delete/:id', deleteUserController)
 
 //Protected Routes - All protected functions are functions that has to be requested using the API (rout) protected
-userRoutes.post('/login', authMiddleware, loginUserController)
+userRoutes.post('/login', /* authMiddleware, */ loginUserController)
 
 export default userRoutes;
